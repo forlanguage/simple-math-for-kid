07897a37d24feb6ca34811cc7d94c4e72ae8206c
Cuong Truong Van <cuongtv.ee@gmail.com>
2026-08-08 14:00:08 +0700

    Connect Grade 1 interactive games

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Level1Games from './Level1Games.jsx'
 import './styles.css'
 import './learning.css'
 import './interactive.css'
+import './level1-games.css'
+
+function Root(){
+  const [games,setGames]=useState(false)
+  if(games) return <Level1Games onBack={()=>setGames(false)}/>
+  return <div className="interactive-root"><App/><button className="interactive-launch" onClick={()=>setGames(true)}>🎮 Bài tương tác Lớp 1</button></div>
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
 )
